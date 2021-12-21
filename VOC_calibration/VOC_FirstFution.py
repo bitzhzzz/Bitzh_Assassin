38b04d56963f9c9cdaf69e682b471c19894de093
zzz <1311569274@qq.com>
2021-12-21 15:36:25 +0800

    1

--- VOC_calibration/VOC_FirstFution.py
+++ VOC_calibration/VOC_FirstFution.py
@@ -2,45 +2,46 @@ import os
 import random
 import numpy as np
 
+#xml文件的存放地址
 xmlfilter = r'./VOC_storage/VOC2021/Annotations'
+#处理后文件的存放地址
 savebasefilter = r'./VOC_storage/VOC2021/ImageSets'
 
+#训练数据中推理和训练的百分比
 trainval_percent = 1
+#训练数据中训练的百分比
 train_percent = 1
 
 temp_xml = os.listdir(xmlfilter)
-
-total_xml=[]
+total_xml = []
 for xml in temp_xml:
     if xml.endswith('.xml'):
         total_xml.append(xml)
 
 num = len(total_xml)
-list =range(num)
-
+list = range(num)
 trainval_num = int(num*trainval_percent)
 train_num = int(trainval_num*train_percent)
-
 trainval_list = random.sample(list, trainval_num)
 train_list = random.sample(trainval_list,train_num)
 
-trainval_filters = open(os.path.join(savebasefilter,'trainval.txt'),'w')
-train_filters = open(os.path.join(savebasefilter,'train.txt'),'w')
-val_filters = open(os.path.join(savebasefilter,'val.txt'),'w')
-test_filters = open(os.path.join(savebasefilter,'test.txt'),'w')
+trainval_filter = open(os.path.join(savebasefilter,'trainval.txt'),'w')
+train_filter = open(os.path.join(savebasefilter,'train.txt'),'w')
+val_filter = open(os.path.join(savebasefilter,'val.txt'),'w')
+test_filter = open(os.path.join(savebasefilter,'test.txt'),'w')
 
 for i in list:
-    name = total_xml[i][:-4]+'\n'
+    num = total_xml[i][:-4]+'\n'
     if i in trainval_list:
-        trainval_filters.write(name)
+        trainval_filter.write(num)
         if i in train_list:
-            train_filters.write(name)
+            train_filter.write(num)
         else:
-            val_filters.write(name)
+            val_filter.write(num)
     else:
-        test_filters.write(name)
+        test_filter.write(num)
 
-trainval_filters.close()
-train_filters.close()
-val_filters.close()
-test_filters.close()
+trainval_filter.close()
+train_filter.close()
+val_filter.close()
+test_filter.close()
